1fe7e951bf2b4130bb62d004fbbc1b1a4f8d6c8a
dohee <dohee12040@gmail.com>
2025-02-25 18:17:17 +0900

    섹션5 슬라이드 수정

--- js/layout_jq.js
+++ js/layout_jq.js
@@ -16,6 +16,7 @@ $(function () {
     dotsClass: "slick-dots", //아래 나오는 페이지네이션(점) css class 지정
     draggable: true, //드래그 가능 여부
     variableWidth: true,
+    centerMode: true, // 카드가 화면 중앙에 오도록 설정
 
     responsive: [
       // 반응형 웹 구현 옵션
@@ -57,5 +58,44 @@ $(function () {
       reviewCards[index].classList.add("active");
       reviewImages[index].classList.add("active"); // 해당 이미지에만 'active' 클래스 추가
     });
+  });
+
+  //섹션5
+
+  $("#sec5_slider-div").slick({
+    slide: "div",
+    infinite: true, // 무한 슬라이드
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 1000, // 전환 속도
+    autoplay: true, // 자동 슬라이드
+    autoplaySpeed: 2000, // 자동 슬라이드 간격
+    pauseOnHover: true, // 마우스 호버 시 슬라이드 멈추기
+    vertical: false,
+    arrows: false, // 화살표 숨기기
+    dots: true, // 하단 점 네비게이션 표시
+    draggable: true, // 드래그 가능
+    variableWidth: true, // 카드 너비 자동 조정
+    cssEase: "ease-out", // 부드러운 전환 효과
+    centerMode: true, // 카드가 화면 중앙에 오도록 설정
+    centerPadding: "0", // 슬라이드 양옆에 여백 없애기
+
+    responsive: [
+      // 반응형 웹 구현 옵션
+      {
+        breakpoint: 960, //화면 사이즈 960px
+        settings: {
+          //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768, //화면 사이즈 768px
+        settings: {
+          //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+          slidesToShow: 2,
+        },
+      },
+    ],
   });
 });
