3b25257bb513ff851eb86dd28016ca1b9bf2206d
dohee <dohee12040@gmail.com>
2025-02-26 18:16:52 +0900

    반응형 laptop사이즈 수정중

--- js/layout.js
+++ js/layout.js
@@ -1,6 +1,14 @@
-document
-  .getElementById("hamburger-icon")
-  .addEventListener("click", function () {
-    const menu = document.getElementById("mobile_menu");
+document.addEventListener("DOMContentLoaded", function () {
+  const menu = document.getElementById("mobile_menu");
+  const hamburgerIcon = document.getElementById("hamburger-icon");
+
+  // 페이지 로드 시 메뉴를 숨기기 위해 기본 스타일을 확인하고 설정
+  if (menu.style.display === "") {
+    menu.style.display = "none"; // 메뉴가 "none"으로 시작하도록 설정
+  }
+
+  hamburgerIcon.addEventListener("click", function () {
+    // 메뉴가 보이면 숨기고, 숨겨져 있으면 보이도록 처리
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   });
+});
