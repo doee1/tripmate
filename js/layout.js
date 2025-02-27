ae2271c76808c7a17208a72cca49cb0969aa9b12
dohee <dohee12040@gmail.com>
2025-02-27 18:13:01 +0900

    반응형 메인페이지

--- js/layout.js
+++ js/layout.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const menu = document.getElementById("mobile_menu");
   const hamburgerIcon = document.getElementById("hamburger-icon");
 
-  // 페이지 로드 시 메뉴를 숨기기 위해 기본 스타일을 확인하고 설정
+  // 페이지 로드 시 메뉴를 숨기기 위해 기본 스타일을 설정
   if (menu.style.display === "") {
     menu.style.display = "none"; // 메뉴가 "none"으로 시작하도록 설정
   }
